f0ba1e43eb4b82a74ea93ffcd6f2cf7aad6827c9
Leonardo Huttner <54815061+leonardohuttner@users.noreply.github.com>
2023-01-19 13:54:21 -0300

    alteraçao api heroku para fly.io

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,7 +8,7 @@ import axios from "axios";
 // "export default () => {}" function below (which runs individually
 // for each client)
 const apiBlockChain = axios.create({
-  baseURL: "https://api-cripto-info.herokuapp.com/",
+  baseURL: "https://api-cript.fly.dev/",
 });
 
 export default boot(({ app }) => {
